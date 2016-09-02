63abbf75a582f5fef8b6441a32878758d9242960
liubang <it.liubang@qq.com>
2016-09-02 12:25:22 +0800

    t

--- webdav.c
+++ webdav.c
@@ -37,7 +37,7 @@
 zend_class_entry *webdav_ce;
 
 static int error(char *err){
-	perror(err);
+	php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s", err);
 	exit(EXIT_FAILURE);
 }
 
@@ -54,7 +54,9 @@ char *file_content(char * file_location){
 	fseek(shell, 0, SEEK_SET);
 
 	buffer = (char *)malloc(size+1);
-	fread(buffer,1,size,shell);
+	if (fread(buffer,1,size,shell) == 0) {
+		error("empty file");
+	}
 	fclose(shell);
 	return buffer;
 }
@@ -85,13 +87,13 @@ static int request(char *target, char *file, char *create, char *path, char **re
 
 	msocket = socket(AF_INET, SOCK_STREAM, 0);
 	if(msocket == -1)
-		error("Fail to create socket ");
+		error("Fail to create socket");
 
 	struct hostent *host;
 	host = gethostbyname(target);
 
 	if(host == NULL)
-		error("Fail to gethostbyname ");
+		error("Fail to gethostbyname");
 
 	addr.sin_family 	= host->h_addrtype;
 	addr.sin_port		= htons(80);
@@ -102,12 +104,12 @@ static int request(char *target, char *file, char *create, char *path, char **re
 	if(connect(msocket,(struct sockaddr*)&addr,sizeof(addr)) == -1)
 		error("Fail to connect");
 
-	char *put = malloc(1024);
+	char *put = malloc(200 + size);
 
 	sprintf(put,"PUT %s%s HTTP/1.1\r\nContent-Length: %d\r\nHost: %s\r\nConnection: close\r\n\r\n%s\r\n\r\n\r\n",path,create,size,target,conteudo);
 
 	if(send(msocket,put,strlen(put),0) < 0)
-		error("\033[1;31m[-]\033[0m fail to make request");
+		error("fail to make request");
 
 	while((recebidos = recv(msocket,resposta,5000,0))){
 		resposta[recebidos] = '\0';
