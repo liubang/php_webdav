4982635246c668ea4a159f340856a355519f0a38
liubang <it.liubang@qq.com>
2016-09-09 18:04:39 +0800

    删除不用的micro

--- php_webdav.h
+++ php_webdav.h
@@ -25,7 +25,6 @@ extern zend_module_entry webdav_module_entry;
 #define PHP_WEBDAV_VERSION 				"0.1.0"
 #define PHP_WEBDAV_AUTHOR				"liubang <it.liubang@gmail.com>"
 #define PROPERTIES_HOST					"_host"
-#define WEBDAV_SOCKET_DESCROPTOR_NAME	"socket"
 
 #define BUF_SIZE 	20480
 #define SOCK_PORT	80
